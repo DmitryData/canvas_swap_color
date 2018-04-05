17cbc90efac7c3b89f844d76a9434b92aba05e6d
Karpovich Dmitry <gameminichannel@gmail.com>
2018-04-05 07:50:28 +0300

    Initial commit

---
+++ app/src/main/java/CustomViewClass/MyView.java
@@ -0,0 +1,10 @@
+package CustomViewClass;
+
+/**
+ * Created by Lenovo on 05.04.2018.
+ * Edited by Karpovich Dmitry.
+ */
+
+public class MyView {
+
+}
---
+++ app/src/main/java/lesson1/canvas_square_swap_color/MainActivity.java
@@ -0,0 +1,13 @@
+package lesson1.canvas_square_swap_color;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
